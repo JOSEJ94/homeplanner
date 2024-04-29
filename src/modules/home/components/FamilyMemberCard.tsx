424a64778a06bc46e7efa51f95ef6ed3b7d74a3d
Jose Rojas <josejrojasc@hotmail.es>
2024-04-29 00:24:53 -0600

    Add group role description in family card

--- src/modules/home/components/FamilyMemberCard.tsx
+++ src/modules/home/components/FamilyMemberCard.tsx
@@ -3,6 +3,7 @@ import {
   PressableProps,
   StyleProp,
   StyleSheet,
+  View,
   ViewStyle,
 } from 'react-native';
 import React, {useMemo} from 'react';
@@ -13,6 +14,7 @@ import {useTheme} from '@react-navigation/native';
 import {GroupMemberFragment, GroupStatus} from '../../../graphql/generated';
 import {firebase} from '@react-native-firebase/auth';
 import {getStaticImageName} from '../../../shared/utils/Image.utils';
+import {GROUP_ROLE_LABELS} from '../../../shared/utils/GroupRole.utils';
 
 interface FamilyMemberCardProps extends PressableProps {
   member: GroupMemberFragment;
@@ -33,10 +35,20 @@ const FamilyMemberCard = ({member, style, ...rest}: FamilyMemberCardProps) => {
           uri: imageSource,
         }}
       />
-      <Typography>
-        {member.user.name} {currentUser && '(You)'}{' '}
-        {member.status === GroupStatus.Invited && '(Invited)'}
-      </Typography>
+      <View>
+        <Typography>
+          {member.user.name}
+          {currentUser && (
+            <Typography style={styles.roleTxt}> (You)</Typography>
+          )}{' '}
+          {member.status === GroupStatus.Invited && (
+            <Typography style={styles.invitedTxt}>(Invited)</Typography>
+          )}
+        </Typography>
+        <Typography style={styles.roleTxt}>
+          {GROUP_ROLE_LABELS[member.role]}
+        </Typography>
+      </View>
     </Pressable>
   );
 };
@@ -49,5 +61,12 @@ const createStyles = (theme: AppTheme) =>
       flexDirection: 'row',
       padding: theme.spacing,
       alignItems: 'center',
+    },
+    invitedTxt: {
+      color: theme.colors.primary,
+    },
+    roleTxt: {
+      fontSize: 14,
+      color: theme.text.subtle,
     },
   });
---
+++ src/shared/utils/GroupRole.utils.ts
@@ -0,0 +1,7 @@
+import {GroupRole} from '../../graphql/generated';
+
+export const GROUP_ROLE_LABELS = {
+  [GroupRole.Admin]: 'Admin',
+  [GroupRole.Editor]: 'Editor',
+  [GroupRole.Member]: 'Member',
+};
